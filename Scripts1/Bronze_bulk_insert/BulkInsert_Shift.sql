86c04f2313eb23117f52f8104d3e8c6d6c26afab
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 15:53:39 +0100

    Create BulkInsert_Shift.sql

---
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Shift.sql
@@ -0,0 +1,19 @@
+/*
+========================================================
+Author:        Khadija Bouchhima
+Date:          2025-12-01
+Environment:   Bronze Layer – Bloom Coffee Shop
+Purpose:       Bulk insert from shift.csv into Bronze_Shift
+========================================================
+*/
+
+
+BULK INSERT Bronze.Bronze_Shift
+FROM 'C:\Path\To\CSV\shift.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TEXTQUALIFIER = '"',
+    CODEPAGE = '65001'
+);
