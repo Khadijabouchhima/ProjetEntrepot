8f0a7c3aad959d0a3ba1914f097216ef7c92efc7
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 16:13:45 +0100

    Update BulkInsert_Inventory.sql

--- Scripts1/Bronze_bulk_insert/BulkInsert_Inventory.sql
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Inventory.sql
@@ -4,8 +4,12 @@ Author:        Khadija Bouchhima
 Date:          2025-12-01
 Environment:   Bronze Layer – Bloom Coffee Shop
 Purpose:       Bulk insert from inventory.csv into Bronze_Inventory
+Notes:         Truncates the table before loading to ensure fresh data
 ========================================================
 */
+
+TRUNCATE TABLE Bronze.Bronze_Inventory;
+GO
 
 BULK INSERT Bronze.Bronze_Inventory
 FROM 'C:\Path\To\CSV\inventory.csv'
@@ -14,6 +18,8 @@ WITH (
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n',
     TEXTQUALIFIER = '"',
-    CODEPAGE = '65001'
+    CODEPAGE = '65001',
     TABLOCK
 );
+GO
+
