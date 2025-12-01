eaaa5e09711d93ef64d2acc6a2b41706497fe290
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 15:52:29 +0100

    Create BulkInsert_Rota.sql

---
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Rota.sql
@@ -0,0 +1,18 @@
+/*
+========================================================
+Author:        Khadija Bouchhima
+Date:          2025-12-01
+Environment:   Bronze Layer – Bloom Coffee Shop
+Purpose:       Bulk insert from rota.csv into Bronze_Rota
+========================================================
+*/
+
+BULK INSERT Bronze.Bronze_Rota
+FROM 'C:\Path\To\CSV\rota.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TEXTQUALIFIER = '"',
+    CODEPAGE = '65001'
+);
