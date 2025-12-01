ca6995c038e1a15eb4293239c2f2c08d6dde0f95
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 15:54:54 +0100

    Create BulkInsert_Staff.sql

---
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Staff.sql
@@ -0,0 +1,19 @@
+/*
+========================================================
+Author:        Khadija Bouchhima
+Date:          2025-12-01
+Environment:   Bronze Layer – Bloom Coffee Shop
+Purpose:       Bulk insert from staff.csv into Bronze_Staff
+========================================================
+*/
+
+
+BULK INSERT Bronze.Bronze_Staff
+FROM 'C:\Path\To\CSV\staff.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TEXTQUALIFIER = '"',
+    CODEPAGE = '65001'
+);
