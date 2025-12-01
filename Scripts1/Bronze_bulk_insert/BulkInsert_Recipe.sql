0f8e071bb5d3df9e7ad611bc3900ea5590de4635
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 16:12:15 +0100

    Update BulkInsert_Recipe.sql

--- Scripts1/Bronze_bulk_insert/BulkInsert_Recipe.sql
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Recipe.sql
@@ -4,9 +4,13 @@ Author:        Khadija Bouchhima
 Date:          2025-12-01
 Environment:   Bronze Layer – Bloom Coffee Shop
 Purpose:       Bulk insert from recipe.csv into Bronze_Recipe
+Notes:         Truncates the table before loading to ensure fresh data
 ========================================================
 */
 
+
+TRUNCATE TABLE Bronze.Bronze_Recipe;
+GO
 
 BULK INSERT Bronze.Bronze_Recipe
 FROM 'C:\Path\To\CSV\recipe.csv'
@@ -15,6 +19,8 @@ WITH (
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n',
     TEXTQUALIFIER = '"',
-    CODEPAGE = '65001'
+    CODEPAGE = '65001',
     TABLOCK
 );
+GO
+
