a9e2fccc1d35c44ee5638dd05d3193fa15b764e2
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 16:12:46 +0100

    Update BulkInsert_Orders.sql

--- Scripts1/Bronze_bulk_insert/BulkInsert_Orders.sql
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Orders.sql
@@ -4,9 +4,13 @@ Author:        Khadija Bouchhima
 Date:          2025-12-01
 Environment:   Bronze Layer – Bloom Coffee Shop
 Purpose:       Bulk insert from orders.csv into Bronze_Orders
+Notes:         Truncates the table before loading to ensure fresh data
 ========================================================
 */
 
+
+TRUNCATE TABLE Bronze.Bronze_Orders;
+GO
 
 BULK INSERT Bronze.Bronze_Orders
 FROM 'C:\Path\To\CSV\orders.csv'
@@ -15,6 +19,8 @@ WITH (
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n',
     TEXTQUALIFIER = '"',
-    CODEPAGE = '65001'
+    CODEPAGE = '65001',
     TABLOCK
 );
+GO
+
