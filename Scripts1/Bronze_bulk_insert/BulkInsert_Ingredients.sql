6f3483e0ee7e07ae89eac9569a1cc4fbdb1e4133
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 15:45:25 +0100

    Create BulkInsert_Ingredients.sql

---
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Ingredients.sql
@@ -0,0 +1,18 @@
+/*
+========================================================
+Author:        Khadija Bouchhima
+Date:          2025-12-01
+Environment:   Bronze Layer – Bloom Coffee Shop
+Purpose:       Bulk insert from ingredients.csv into Bronze_Ingredients
+========================================================
+*/
+
+BULK INSERT Bronze.Bronze_Ingredients
+FROM 'C:\Path\To\CSV\ingredients.csv'  -- Update path as needed
+WITH (
+    FIRSTROW = 2,       -- Skip header row
+    FIELDTERMINATOR = ',',  
+    ROWTERMINATOR = '\n',
+    TEXTQUALIFIER = '"',
+    CODEPAGE = '65001'   -- UTF-8
+);
