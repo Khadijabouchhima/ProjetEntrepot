91ae2be9b3e515da58d1c9980b07b6deb11316da
Khadijabouchhima <khadija204hd@gmail.com>
2025-12-01 16:09:56 +0100

    Update BulkInsert_Staff.sql

--- Scripts1/Bronze_bulk_insert/BulkInsert_Staff.sql
+++ Scripts1/Bronze_bulk_insert/BulkInsert_Staff.sql
@@ -4,9 +4,12 @@ Author:        Khadija Bouchhima
 Date:          2025-12-01
 Environment:   Bronze Layer – Bloom Coffee Shop
 Purpose:       Bulk insert from staff.csv into Bronze_Staff
+Notes:         Truncates the table before loading to ensure fresh data
 ========================================================
 */
 
+TRUNCATE TABLE Bronze.Bronze_Staff;
+GO
 
 BULK INSERT Bronze.Bronze_Staff
 FROM 'C:\Path\To\CSV\staff.csv'
@@ -15,6 +18,7 @@ WITH (
     FIELDTERMINATOR = ',',
     ROWTERMINATOR = '\n',
     TEXTQUALIFIER = '"',
-    CODEPAGE = '65001'
+    CODEPAGE = '65001',
     TABLOCK
 );
+GO
